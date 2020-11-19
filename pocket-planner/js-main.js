d08aaf08009099671f7bc4bb70c8844e5eeab80b
Melissa Gabriele <meg394@drexel.edu>
2020-11-19 15:28:53 -0500

    styling fixed for safari
    functionality still posting to url ⍨

--- pocket-planner/js-main.js
+++ pocket-planner/js-main.js
@@ -1,3 +1,94 @@
+/* -------- STORAGE ------- */
+
+// for each section of the form
+var titleArray = new Array();
+var dateArray = new Array();
+var timeArray = new Array();
+var colorArray = new Array();
+var detailsArray = new Array();
+
+//array integer number
+var theIndex = 0;
+
+//read data
+function readData() {
+    //to make sure its working
+    console.log("read-data-called")
+    if (typeof (Storage) !== "undefined") {
+       titleStr = localStorage.titleArray;
+       dateStr = localStorage.dateArray;
+       timeStr = localStorage.timeArray;
+       colotStr = localStorage.colorArray;
+       detailsStr = localStorage.detailsArray;
+       theIndexStr = localStorage.myIndex;
+       //to make sure its working
+       console.log(titleStr + dateStr + timeStr + colotStr + detailsStr + theIndexStr)
+
+       if (typeof (titleStr) !== "undefined") {
+           //data string to array
+            titleArray = titleStr.split(",");
+            dateArray = dateStr.split(",");
+            timeArray = timeStr.split(",");
+            colorArray = colorStr.split(",");
+            detailsArray = detailsStr.split(",");
+            //index string to integer
+            theIndex = parseInt(theIndexStr);
+            //display data
+            taskTitle.value = titleArray[theIndex];
+            taskDate.value = dateArray[theIndex];
+            taskTime.value = timeArray[theIndex];
+            taskColors.value = colorArray[theIndex];
+            taskDetails.value = detailsArray[theIndex];
+            //index result
+            theIndexResult.value = theIndex;
+       } else {
+        //new data
+        taskTitle.value = "";
+        taskDate.value = 0;
+        taskTime.value = 0
+        // taskColors.value = "";
+        taskDetails.value = "";
+        //index result
+        theIndex = 0;
+       }
+    } else {
+        alert ('This browser does NOT support local storage');
+    }
+}
+
+//write data
+function writeData() {
+    console.log("write-data-called");
+    if (typeof (Storage) !== "undefined") {
+        //add variables to end of array
+        titleArray.push = (taskTitle.value);
+        dateArray.push = (taskDate.value);
+        timeArray.push = (taskTime.value);
+        colorArray.push = (taskColors.value);
+        detailsArray.push = (taskDetails.value);
+        //legnth
+        theIndex = titleArray.length - 1;
+        //convert data to strings
+        titleStr = titleArray.join();
+        dateStr = dateArray.join();
+        timeStr = timeArray.join();
+        colotStr = colorArray.join();
+        detailsStr = detailsArray.join();
+        //save to local storage
+        localStorage.taskTitle = titleStr;
+        localStorage.taskDate = dateStr;
+        localStorage.taskTime = timeStr;
+        localStorage.taskColors = colotStr;
+        localStorage.taskDetails = detailsStr;
+        localStorage.myIndex = theIndex;
+        alert("Record Added");
+    
+    } else {
+        alert ('This browser does NOT support local storage');
+    }
+}
+
+
 /* -------- CURRENT WEEKDAY AND DATE ------- */
 
 //for weekday, month, day, and year
@@ -60,23 +151,49 @@ theMain.onscroll = function() {scrollFunction()};
 
 //function changes header if page has been scrolled
 function scrollFunction() {
-    if (theMain.scrollTop > 40) {
-    topLogo.style.width = "10%";
-    topLogo.style.position = "absolute";
-    topLogo.style.top = "3%";
-    topLogo.style.left = "47%";
-    topLogo.style.paddingTop = ".15rem";
-    theHeader.style.backgroundColor = "var(--lightest2)";
-    } else if (addModal.style.display == "block") {
-    topLogo.style.width = "12%";
-    topLogo.style.position = "absolute";
-    topLogo.style.top = "8%";
-    topLogo.style.left = "47%";
-    topLogo.style.paddingTop = ".15rem";
-    } else  {
-    topLogo.style.width = "25%";
-    topLogo.style.position = "static";
-    theHeader.style.backgroundColor = "unset";
+    if (window.matchMedia("(orientation: portrait)").matches) {
+        if (theMain.scrollTop > 40) {
+            topLogo.style.width = "10%";
+            topLogo.style.position = "absolute";
+            topLogo.style.top = "3%";
+            topLogo.style.left = "47%";
+            topLogo.style.paddingTop = ".15rem";
+            theHeader.style.backgroundColor = "var(--lightest2)";
+            topLogo.style.marginTop = "0";
+            } else if (addModal.style.display == "block") {
+            topLogo.style.width = "12%";
+            topLogo.style.position = "absolute";
+            topLogo.style.top = "8%";
+            topLogo.style.left = "47%";
+            topLogo.style.paddingTop = ".3rem";
+            topLogo.style.marginTop = "0";
+            } else  {
+            topLogo.style.width = "25%";
+            topLogo.style.position = "static";
+            theHeader.style.backgroundColor = "unset";
+            topLogo.style.marginTop = "0";
+            topLogo.style.paddingTop = "0";
+            }
+    } else {
+        if (theMain.scrollTop > 40) {
+            topLogo.style.width = "5%";
+            topLogo.style.position = "absolute";
+            topLogo.style.top = "3%";
+            topLogo.style.left = "47%";
+            topLogo.style.marginTop = ".4rem";
+            theHeader.style.backgroundColor = "var(--lightest2)";
+            } else if (addModal.style.display == "block") {
+            topLogo.style.width = "5%";
+            topLogo.style.position = "absolute";
+            topLogo.style.top = "8%";
+            topLogo.style.left = "47%";
+            topLogo.style.marginTop = "0";
+            } else  {
+            topLogo.style.width = "12%";
+            topLogo.style.position = "static";
+            topLogo.style.marginTop = "-1.5rem";
+            theHeader.style.backgroundColor = "unset";
+            }
     }
 }
 
