d79c00a42fc57f5e5cd7140778c82142a3c8248b
Melissa Gabriele <meg394@drexel.edu>
2020-11-24 18:59:07 -0500

    troubleshooting
    updates

--- pocket-planner/js-main.js
+++ pocket-planner/js-main.js
@@ -1,93 +1,178 @@
-/* -------- STORAGE ------- */
+// /* -------- STORAGE ------- */
 
-// for each section of the form
-var titleArray = new Array();
-var dateArray = new Array();
-var timeArray = new Array();
-var colorArray = new Array();
-var detailsArray = new Array();
+var taskTitleArray = new Array();
+var taskDateArray = new Array();
+var taskTimeArray = new Array();
+var taskDetailsArray = new Array();
+var mainNdx = 0;
 
-//array integer number
-var theIndex = 0;
-
-//read data
 function readData() {
-    //to make sure its working
-    console.log("read-data-called")
-    if (typeof (Storage) !== "undefined") {
-       titleStr = localStorage.titleArray;
-       dateStr = localStorage.dateArray;
-       timeStr = localStorage.timeArray;
-       colotStr = localStorage.colorArray;
-       detailsStr = localStorage.detailsArray;
-       theIndexStr = localStorage.myIndex;
-       //to make sure its working
-       console.log(titleStr + dateStr + timeStr + colotStr + detailsStr + theIndexStr)
+  console.log("readData called");
+  // Does this browser support local storage?
+  if (typeof (Storage) !== "undefined") {
+    // Read data from local storage
+    taskTitleStr = localStorage.my_taskTitle;
+    taskDateStr = localStorage.my_taskDate;
+    taskTimeStr = localStorage.my_taskTime;
+    taskDetailsStr = localStorage.my_taskDetails;
+    ndxStr = localStorage.my_Ndx;
 
-       if (typeof (titleStr) !== "undefined") {
-           //data string to array
-            titleArray = titleStr.split(",");
-            dateArray = dateStr.split(",");
-            timeArray = timeStr.split(",");
-            colorArray = colorStr.split(",");
-            detailsArray = detailsStr.split(",");
-            //index string to integer
-            theIndex = parseInt(theIndexStr);
-            //display data
-            taskTitle.value = titleArray[theIndex];
-            taskDate.value = dateArray[theIndex];
-            taskTime.value = timeArray[theIndex];
-            taskColors.value = colorArray[theIndex];
-            taskDetails.value = detailsArray[theIndex];
-            //index result
-            theIndexResult.value = theIndex;
-       } else {
-        //new data
-        taskTitle.value = "";
-        taskDate.value = 0;
-        taskTime.value = 0
-        // taskColors.value = "";
-        taskDetails.value = "";
-        //index result
-        theIndex = 0;
-       }
+    console.log("taskTitleStr is .. " + taskTitleStr);
+    console.log("taskDateStr is .. " + taskDate);
+    console.log("taskTimeStr is .. " + taskTime);
+    console.log("taskDetailsStr is .. " + taskDetails);
+    console.log("ndxStr is .. " + ndxStr);
+
+    if (typeof (taskTitleStr) !== "undefined") {
+      // Convert data string into array
+      taskTitleArray = taskTitleStr.split(",");
+      taskDateArray = taskDateStr.split(",");
+      taskTimeArray = taskTimeStr.split(",");
+      taskDetailsArray = taskDetailsStr.split(",");
+      // Convert Ndx string into integer
+      mainNdx = parseInt(ndxStr);
+      // Display data screen
+      taskTitle.value = taskTitleArray[mainNdx];
+      taskDate.value = taskDateArray[mainNdx];
+      taskTime.value = taskTimeArray[mainNdx];
+      taskDetails.value = taskDetailsArray[mainNdx];
+      ndx_result.value = mainNdx;
     } else {
-        alert ('This browser does NOT support local storage');
+      // Initize data if it is empty/invalid
+      taskTitle.value = "";
+      taskDate.value = "";
+      taskTime.value = "";
+      taskDetails.value = "";
+      mainNdx = 0;
     }
+  } else {
+    // Sorry! No Web Storage support..
+    alert('This browser does NOT support local storage');
+  }
 }
 
-//write data
 function writeData() {
-    console.log("write-data-called");
-    if (typeof (Storage) !== "undefined") {
-        //add variables to end of array
-        titleArray.push = (taskTitle.value);
-        dateArray.push = (taskDate.value);
-        timeArray.push = (taskTime.value);
-        colorArray.push = (taskColors.value);
-        detailsArray.push = (taskDetails.value);
-        //legnth
-        theIndex = titleArray.length - 1;
-        //convert data to strings
-        titleStr = titleArray.join();
-        dateStr = dateArray.join();
-        timeStr = timeArray.join();
-        colotStr = colorArray.join();
-        detailsStr = detailsArray.join();
-        //save to local storage
-        localStorage.taskTitle = titleStr;
-        localStorage.taskDate = dateStr;
-        localStorage.taskTime = timeStr;
-        localStorage.taskColors = colotStr;
-        localStorage.taskDetails = detailsStr;
-        localStorage.myIndex = theIndex;
-        alert("Record Added");
-    
+  console.log("writeData called");
+  if (typeof (Storage) !== "undefined") {
+
+    //check the date
+    setTaskDate = Date.parse(taskDate.value);
+    console.log("date entered = " + setTaskDate);
+    if (setTaskDate >= theDate) {
+        if (taskTitle.value !== "") {
+             // Add data to array
+            taskTitleArray.push(taskTitle.value);
+            taskDateArray.push(taskDate.value);
+            taskTimeArray.push(taskTime.value);
+            taskDetailsArray.push(taskDetails.value);
+            // Increment array index number
+            mainNdx = taskTitleArray.length - 1;
+            // Convert arrays into data strings
+            taskTitleStr = taskTitleArray.join();
+            taskDateStr = taskDateArray.join();
+            taskTimeStr = taskTimeArray.join();
+            taskDetailsStr = taskDetailsArray.join();
+            // save data strings to local storage
+            localStorage.my_taskTitle = taskTitleStr;
+            localStorage.my_taskDate = taskDateStr;
+            localStorage.my_taskTime = taskTimeStr;
+            localStorage.my_taskDetails = taskDetailsStr;
+            localStorage.my_Ndx = mainNdx;
+            //
+            ndx_result.value = mainNdx;
+            alert('Record ADDED.');
+            createTask();
+        } else {
+            alert('Please add a title');
+        }
     } else {
-        alert ('This browser does NOT support local storage');
+        alert('This date has already passed');
     }
+  } else {
+    // Sorry! No Web Storage support..
+    alert('This browser does NOT support local storage');
+  }
 }
 
+/* -------- TASK COLOR ------- */
+var taskColor = document.getElementsByClassName("taskColors");
+
+for (var i = 0; i < taskColor.length; i++) {
+    taskColor[i].addEventListener("click", function(){
+        whichColor = event.target.id;
+        if (whichColor == "red") {
+            theTaskColor = "var(--red)";
+        } else if (whichColor == "orange") {
+            theTaskColor = "var(--orange)";
+        } else if (whichColor == "yellow") {
+            theTaskColor = "var(--yellow)";
+        } else if (whichColor == "green") {
+            theTaskColor = "var(--green)";
+        } else if (whichColor == "blue") {
+            theTaskColor = "var(--blue)";
+        } else if (whichColor == "purple") {
+            theTaskColor = "var(--purple)";
+        } else {
+            alert("Please select a color");   
+        }
+    }, false);
+}
+
+/* -------- TASK CREATION ------- */
+let mainContent = document.getElementById("main-content");
+var taskCount = 0;
+function createTask() {
+    //create task div
+    var newTask = document.createElement('div');
+    newTask.className = 'task';
+    mainContent.appendChild(newTask);
+    //create color div
+    var newTaskColor = document.createElement('div');
+    newTaskColor.className = 'color';
+    //to select correct color
+    newTaskColor.style.backgroundColor = theTaskColor;
+    newTask.appendChild(newTaskColor);
+    //create paragraph div and fill with title
+    var newTaskP = document.createElement('p');
+    newTaskP.innerHTML = taskTitleArray[taskCount];
+    newTask.appendChild(newTaskP);
+    //bring up modal on click
+    console.log("childrren " + mainContent.children);
+    newTaskP.onclick = function() {
+        console.log("hello!! " + evt);
+        //display modal
+        // addModal.style.display = "block";
+        // // Update web form fields with new values
+        // taskTitle.value = taskTitleArray[0];
+        // taskDate.value = taskDateArray[0];
+        // taskTime.value = taskTimeArray[0];
+        // taskDetails.value = taskDetailsArray[0];
+        // ndx_result.value = 0;
+        // // Save current index to local storage
+        // localStorage.my_Ndx = 0;
+    };
+    //create h3 div and fill with time
+    var newTaskH3 = document.createElement('h3');
+    newTaskH3.innerHTML = taskTimeArray[taskCount];
+    newTask.appendChild(newTaskH3);
+    //create checkbox div
+    var newTaskCheck = document.createElement('div');
+    newTaskCheck.className = 'checkbox';
+    //checkbox functionality
+    newTaskCheck.onclick = function() {
+        event.target.parentNode.classList.toggle("task-toggle");
+        event.target.firstChild.classList.toggle("check-toggle");
+    };
+    newTaskCheck.className = 'checkbox';
+    newTask.appendChild(newTaskCheck);
+    //checkbox image
+    var newTaskGif = document.createElement('img');
+    newTaskGif.src="img/check.gif";
+    newTaskGif.className = 'checkimg';
+    newTaskCheck.appendChild(newTaskGif);
+    //increase count
+    taskCount++;
+}
 
 /* -------- CURRENT WEEKDAY AND DATE ------- */
 
@@ -126,6 +211,7 @@ var addModal = document.getElementById("add-modal");
 
 //event listener - modal off, modal on for click, header change
 addButton.addEventListener("click", () => {
+    taskTitle.value = "";
     addButton.style.display = "none";
     addModal.style.display = "block";
     scrollFunction();
@@ -140,8 +226,6 @@ xButton.addEventListener("click", () => {
 
 /* -------- HEADER RESIZE ------- */
 
-//get logo
-let topLogo = document.getElementById("top-logo");
 //get header
 let theHeader = document.getElementById("header");
 //get main container
@@ -151,70 +235,11 @@ theMain.onscroll = function() {scrollFunction()};
 
 //function changes header if page has been scrolled
 function scrollFunction() {
-    if (window.matchMedia("(orientation: portrait)").matches) {
-        if (theMain.scrollTop > 40) {
-            topLogo.style.width = "10%";
-            topLogo.style.position = "absolute";
-            topLogo.style.top = "3%";
-            topLogo.style.left = "47%";
-            topLogo.style.paddingTop = ".15rem";
-            theHeader.style.backgroundColor = "var(--lightest2)";
-            topLogo.style.marginTop = "0";
-            } else if (addModal.style.display == "block") {
-            topLogo.style.width = "12%";
-            topLogo.style.position = "absolute";
-            topLogo.style.top = "8%";
-            topLogo.style.left = "47%";
-            topLogo.style.paddingTop = ".3rem";
-            topLogo.style.marginTop = "0";
-            } else  {
-            topLogo.style.width = "25%";
-            topLogo.style.position = "static";
-            theHeader.style.backgroundColor = "unset";
-            topLogo.style.marginTop = "0";
-            topLogo.style.paddingTop = "0";
-            }
-    } else {
-        if (theMain.scrollTop > 40) {
-            topLogo.style.width = "5%";
-            topLogo.style.position = "absolute";
-            topLogo.style.top = "3%";
-            topLogo.style.left = "47%";
-            topLogo.style.marginTop = ".4rem";
-            theHeader.style.backgroundColor = "var(--lightest2)";
-            } else if (addModal.style.display == "block") {
-            topLogo.style.width = "5%";
-            topLogo.style.position = "absolute";
-            topLogo.style.top = "8%";
-            topLogo.style.left = "47%";
-            topLogo.style.marginTop = "0";
-            } else  {
-            topLogo.style.width = "12%";
-            topLogo.style.position = "static";
-            topLogo.style.marginTop = "-1.5rem";
-            theHeader.style.backgroundColor = "unset";
-            }
-    }
-}
-
-/* -------- CHECK BUTTON FUNCTIONALITY ------- */
-// let checkBox = document.getElementById("checkbox");
-// let checkImg = document.getElementById("checkimg");
-
-// checkBox.addEventListener("click", () => {
-//     checkImg.style.display = "block";
-// }, false);
-
-
-// var checkBox = document.getElementsByClassName("checkbox");
-
-// function addCheck(i) {
-//     // checkImg.style.display = "block";
-//     i.console.log(i);
-// };
-
-// for (var i = 0; i < checkBox.length; i++) {
-//     checkBox[i].addEventListener('click', addCheck(i), false);
-// }
+    if (theMain.scrollTop > 30) {
+        theHeader.style.backgroundColor = "var(--lightest2)";
+        } else  {
+        theHeader.style.backgroundColor = "unset";
+        }
+} 
 
 
